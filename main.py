4b7732995288ebb031e907800511822867bb2866
7IronSnow7 <divan.debruin25@gmail.com>
2025-05-14 19:19:08 -0400

    Created the logic for points and line methods, played around with it, creating a right angled triangle

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
+    
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
+    
+    l2 = Line(Point(50, 50), Point(50, 400))
+    win.draw_line(l2, "red")
+    
+    l3 = Line(Point(50, 400), Point(400, 400))
+    win.draw_line(l3, "black")
+    
+    
+    win.wait_for_close() 
     
 main()
