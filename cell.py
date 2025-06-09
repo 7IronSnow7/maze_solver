0f66e1bd4f15c45db8bac7cb1113236220bedbf8
7IronSnow7 <divan.debruin25@gmail.com>
2025-06-09 16:25:47 -0400

    Added the cell.py module and the maze.py module, updated the main to run the code

--- cell.py
+++ cell.py
@@ -29,4 +29,17 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line, "black")
+            
+    def draw_move(self, to_cell, undo=False):
+        center_point_x_start = (self._x1 + self._x2) / 2
+        center_point_y_start = (self._y1 + self._y2) / 2
+        center_point_start = Point(center_point_x_start, center_point_y_start)
+
+        center_point_x_end = (to_cell._x1 + to_cell._x2) / 2
+        center_point_y_end = (to_cell._y1 + to_cell._y2) / 2
+        center_point_end = Point(center_point_x_end, center_point_y_end)
         
+        line = Line(center_point_start, center_point_end)
+        color = "gray" if undo else "red"
+        self._win.draw_line(line, color)
+                
